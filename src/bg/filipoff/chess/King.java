25952b36044adace744e1be416d746b9f346167e
Filipoff <marto_1337@hotmail.com>
2017-09-25 18:00:57 +0300

    Add King class, add map for FigureShortStringRepresentation in Constants, add figuresInPlay list in Player

---
+++ src/bg/filipoff/chess/King.java
@@ -0,0 +1,48 @@
+package bg.filipoff.chess;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class King extends Figure {
+
+	public King(Player owner, Field field) {
+		super(owner, field);
+	}
+
+	@Override
+	public String toString() {
+		return super.getOwner().getColor()
+				+ Constants.FigureStringRepresentation.get(this);
+	}
+
+	@Override
+	public List<Field> getPossibleFieldsToMove() {
+
+		List<Field> possibleFieldsToMove = new LinkedList<Field>();
+
+		for (int i = -1; i <= 1; i++) {
+
+			for (int j = -1; j <= 1; j++) {
+
+				if (i == 0 && j == 0)
+					continue;
+
+				try {
+					Field possible = this
+							.getField()
+							.getBoard()
+							.getFieldAt(
+									this.getField().getPosition().getRow() + i,
+									this.getField().getPosition().getColumn()
+											+ j);
+					if (possible != null)
+						possibleFieldsToMove.add(possible);
+
+				} catch (IllegalArgumentException e) {
+					System.out.println("Exception caught in King");
+				}
+			}
+		}
+		return possibleFieldsToMove;
+	}
+}
